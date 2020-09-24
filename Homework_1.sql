8b003159e7f445819a06ee2783972ae4c3944356
ncklasala <33462642+ncklasala@users.noreply.github.com>
2020-09-24 19:30:48 -0400

    Update Homework_1.sql
    Added guests.

--- Homework_1.sql
+++ Homework_1.sql
@@ -1,6 +1,17 @@
 
-
+DROP TABLE IF EXISTS Location;
 DROP TABLE IF EXISTS Taverns;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Roles;
+DROP TABLE IF EXISTS Rats;
+DROP TABLE IF EXISTS Supplies;
+DROP TABLE IF EXISTS Inventory;
+DROP TABLE IF EXISTS Sales;
+DROP TABLE IF EXISTS Service;
+DROP TABLE IF EXISTS Guests;
+DROP TABLE IF EXISTS Classes;
+
+
 CREATE TABLE Taverns(
 	Id INT IDENTITY(1, 1) PRIMARY KEY,
     LocationId INT,
@@ -8,10 +19,8 @@ CREATE TABLE Taverns(
     OwnerId Int
 );
 
-
 INSERT INTO Taverns (TavernName,LocationId,OwnerId) VALUES ('The Hall',1,1),('Moes',2,2),('Petrocks',1,3),('Petrocks',2,4),('Rat R US',5,5);
 
-DROP TABLE IF EXISTS Users;
 CREATE TABLE Users (
 	Id INT IDENTITY(1, 1) PRIMARY KEY,
 	Name varchar(250),
@@ -20,7 +29,7 @@ CREATE TABLE Users (
 
 INSERT INTO Users (Name,RoleId) VALUES ('Kathy',1),('Moe',1),('Jim',1),('sam',2),('Rat King',1),('Tim', 2),('Bethany',2);
 
-DROP TABLE IF EXISTS Roles;
+
 CREATE TABLE Roles (
 	Id TINYINT IDENTITY(1, 1) PRIMARY KEY,
 	Name varchar(50),
@@ -29,7 +38,7 @@ CREATE TABLE Roles (
 
 INSERT INTO Roles (Name,Description) VALUES ('Owner','The owner of the Tavern'),('Bartender','Server the beer'),('Waiter','Servers Tables'),('Dish washer','Washes dishes'),('Customer','Patron of the Tavern');
 
-DROP TABLE IF EXISTS Location;
+
 CREATE TABLE Location (
 	Id INT IDENTITY(1, 1) PRIMARY KEY,
 	Name varchar(250),
@@ -38,7 +47,7 @@ CREATE TABLE Location (
 INSERT INTO Location (Name) VALUES ('NJ'),('NY'),('MA'),('DE'),('MD'),('VA');
 
 
-DROP TABLE IF EXISTS Rats;
+
 CREATE TABLE Rats (
 	Id TINYINT IDENTITY(1, 1) PRIMARY KEY,
 	Name varchar(250),
@@ -47,7 +56,7 @@ CREATE TABLE Rats (
 
 INSERT INTO Rats (Name) VALUES ('Micky'),('Pikachu'),('Lou'),('Charlie'),('Minnie'),('Ricky'),('Bubbles');
 
-DROP TABLE IF EXISTS Supplies;
+
 CREATE TABLE Supplies (
 	Id TINYINT IDENTITY(1, 1) PRIMARY KEY,
 	Name varchar(250),
@@ -55,7 +64,7 @@ CREATE TABLE Supplies (
 
 INSERT INTO Supplies (Name) VALUES ('Coors'),('Guinness'),('Reds'),('Heineken'),('Blue Moon'),('Popcorn');
 
-DROP TABLE IF EXISTS Inventory;
+
 CREATE TABLE Inventory (
 	Id TINYINT IDENTITY(1, 1) PRIMARY KEY,
     tavernId INT,
@@ -65,7 +74,7 @@ CREATE TABLE Inventory (
 
 INSERT INTO Inventory (tavernId, supplyId) VALUES (1,1),(1,2),(1,3),(2,1),(2,4);
 
-DROP TABLE IF EXISTS Sales;
+
 CREATE TABLE Sales (
 	Id TINYINT IDENTITY(1, 1) PRIMARY KEY,
     tavernId Int,
@@ -77,7 +86,7 @@ CREATE TABLE Sales (
 
 INSERT INTO Sales (tavernId, userId, price, supplyId) VALUES (1,2, 5, 1),(1,2, 50, 2),(2,3, 5, 3),(3,2, 7, 1),(5,2, 9, 4), (4,4, 18, 4);
 
-DROP TABLE IF EXISTS Service;
+
 CREATE TABLE Service (
 	Id TINYINT IDENTITY(1, 1) PRIMARY KEY,
     name varchar(250),
@@ -91,3 +100,32 @@ INSERT INTO Service (name, tavernId, price, status) VALUES ('Bar Service',1,NULL
 
 ALTER TABLE Taverns ADD FOREIGN KEY (LocationId) REFERENCES Location(Id);
 
+CREATE TABLE Guests (
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+    name varchar(250),
+    notes varchar(MAX),
+    birthday varchar(50),
+    cakeday varchar(50),
+    status varchar(250),
+);
+
+INSERT INTO Guests (name,birthday,cakeday, notes, status) VALUES ('Nick','5/26','1/1','Left','Happy');
+
+CREATE TABLE Classes(
+  Id INT IDENTITY(1, 1) PRIMARY KEY,
+  name varchar(250),
+);
+
+INSERT INTO Classes (name) VALUES ('Knight'),('Mage'),('Cleric'),('Ranger'),('Thief'),('Brawler');
+
+
+CREATE TABLE GuestsClasses (
+	Id INT IDENTITY(1, 1) PRIMARY KEY,
+    guestId INT,
+    classId INT,
+    Lvl INT,
+);
+ALTER TABLE GuestsClasses ADD FOREIGN KEY (guestId) REFERENCES Guests(Id);
+ALTER TABLE GuestsClasses ADD FOREIGN KEY (classId) REFERENCES Classes(Id);
+
+INSERT INTO GuestsClasses (guestId, classId, lvl) VALUES (1,2,10);
