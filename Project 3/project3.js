a49160520c4f9057b1fcef66de562c62e33bd6cc
ncklasala <33462642+ncklasala@users.noreply.github.com>
2020-11-02 17:39:52 -0500

    Functional js

--- Project 3/project3.js
+++ Project 3/project3.js
@@ -1,14 +1,19 @@
-function addNum(number1,number2){
 
+function addNum(number1,number2){
+    let sum= number1+number2;
+    return sum;
 }
 
 function subtractNum(number1,number2){
-
+    let sub= number1 - number2;
+    return sub;
 }
 
 function multiplyNum(number1,number2){
-
+    let multiple= number1*number2;
+    return multiple;
 }
 function divideNum(number1,number2){
-
+    let divide= number1*number2;
+    return divide;
 }
